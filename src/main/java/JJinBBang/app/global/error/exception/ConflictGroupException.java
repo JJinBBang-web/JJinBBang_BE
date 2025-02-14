879b122af2f42687218c70d7db000cddeaada3b9
Junho Yu <ryuwon@gnu.ac.kr>
2025-02-15 02:42:49 +0900

    🎉 Init[#2]: 에러 그룹 및 에러 응답 dto 추가

---
+++ src/main/java/JJinBBang/app/global/error/exception/ConflictGroupException.java
@@ -0,0 +1,7 @@
+package JJinBBang.app.global.error.exception;
+
+public abstract class ConflictGroupException extends RuntimeException {
+	public ConflictGroupException(String message) {
+		super(message);
+	}
+}
