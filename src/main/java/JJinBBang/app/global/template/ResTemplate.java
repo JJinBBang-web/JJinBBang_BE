df7f5ed639de682ce4844c9595f27556a87b496f
Junho Yu <ryuwon@gnu.ac.kr>
2025-02-15 02:37:25 +0900

    ✨ Feat[#2]: ResTemplate logging 추가

--- src/main/java/JJinBBang/app/global/template/ResTemplate.java
+++ src/main/java/JJinBBang/app/global/template/ResTemplate.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Getter
 @JsonPropertyOrder({"code","message","data"})
 public class ResTemplate<T> {
@@ -19,11 +20,13 @@ public class ResTemplate<T> {
 		this.code = httpStatus.value();
 		this.message = message;
 		this.data = data;
+		log.info("Response generated: code={}, message={}", code, message);
 	}
 
 	// 실패하였을 경우
 	public ResTemplate(HttpStatus httpStatus, String message) {
 		this.code = httpStatus.value();
 		this.message = message;
+		log.info("Response generated: code={}, message={}", code, message);
 	}
 }
