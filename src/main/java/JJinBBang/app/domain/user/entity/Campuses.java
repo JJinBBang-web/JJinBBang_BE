923eaa1b26c15fbeda3b251b146567382a0e1cb8
Junho Yu <ryuwon@gnu.ac.kr>
2025-02-21 22:10:06 +0900

    ✨ Feat[#7]: Campuses, Universities 연관관계 추가

--- src/main/java/JJinBBang/app/domain/user/entity/Campuses.java
+++ src/main/java/JJinBBang/app/domain/user/entity/Campuses.java
@@ -1,12 +1,17 @@
 package JJinBBang.app.domain.user.entity;
 
 import java.awt.geom.Point2D;
+import java.util.List;
 
+import JJinBBang.app.domain.building.entity.Buildings;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -30,4 +35,13 @@ public class Campuses {
 	private Point2D.Double campusCoordinate; // 캠퍼스 좌표
 
 	private String image; // 캠퍼스 이미지
+
+	// 연관관계 매핑
+	@ManyToOne
+	@JoinColumn(name = "university_id")
+	private Universities university; // 대학교
+
+	@ManyToOne
+	@JoinColumn(name = "building_id")
+	private Buildings building; // 건물
 }
--- src/main/java/JJinBBang/app/domain/user/entity/Universities.java
+++ src/main/java/JJinBBang/app/domain/user/entity/Universities.java
@@ -1,12 +1,15 @@
 package JJinBBang.app.domain.user.entity;
 
 import java.math.BigInteger;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,4 +28,11 @@ public class Universities {
 
 	@Column(nullable = false)
 	private String universityLogo; // 대학교 로고
+
+	// 연관관계 매핑
+	@OneToMany(mappedBy = "university", cascade = CascadeType.ALL)
+	private List<Campuses> campuses; // 캠퍼스
+
+	@OneToMany(mappedBy = "university", cascade = CascadeType.ALL)
+	private List<Users> users; // 유저
 }
