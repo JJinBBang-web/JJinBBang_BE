a7ae26b408bce02ceeec9b1a6d68c480a7c8cc27
Junho Yu <ryuwon@gnu.ac.kr>
2025-02-22 23:28:32 +0900

    ⚡️ Chore[#7]: 불변성 및 관리를 위한 어노테이션 제거

--- src/main/java/JJinBBang/app/domain/building/entity/BuildingLikes.java
+++ src/main/java/JJinBBang/app/domain/building/entity/BuildingLikes.java
@@ -2,24 +2,20 @@ package JJinBBang.app.domain.building.entity;
 
 import JJinBBang.app.domain.user.entity.Users;
 import jakarta.persistence.*;
-        import lombok.*;
+import lombok.*;
 
 @Entity
 @Getter
-@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
-@Table(name = "building_likes")
-//@IdClass(BuildingLikeId.class)
 public class BuildingLikes {
 
     @Id
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "building_id", nullable = false)
-    private Buildings building;
+    private Buildings buildings;
 
     @Id
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private Users user;
 }
--- src/main/java/JJinBBang/app/domain/building/entity/ReviewLikes.java
+++ src/main/java/JJinBBang/app/domain/building/entity/ReviewLikes.java
@@ -2,24 +2,20 @@ package JJinBBang.app.domain.building.entity;
 
 import JJinBBang.app.domain.user.entity.Users;
 import jakarta.persistence.*;
-        import lombok.*;
+import lombok.*;
 
 @Entity
 @Getter
-@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
-@Table(name = "review_likes")
-// @IdClass(ReviewLikeId.class)
 public class ReviewLikes {
 
     @Id
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "review_id", nullable = false)
-    private Reviews review;
+    private Reviews reviews;
 
     @Id
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private Users user;
 }
