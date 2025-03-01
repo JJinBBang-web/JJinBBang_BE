15f098bfa2139194d56134fd100d162b467009d4
circlezero <wy129@naver.com>
2025-03-02 02:35:44 +0900

    :sparkles: Feat[#13]: CustomAccessDeniedHandler 예외처리 강화
    유저의 학교 인증 상태에 따른 다양한 예외를 반환할 수 있도록 보강
    
    url이 요구하는 인증 상태에 따라 유저의 인증 상태가
    - VERIFIED가 아닌 경우 → "학교 인증이 필요합니다."
    - UNVERIFIED가 아닌 경우 →
        - VERIFIED인 경우 → "이미 학교 인증이 완료되었습니다."
        - PENDING인 경우 → "대기 중인 학교 인증 요청이 존재합니다.”
    - PENDING이 아닌 경우 →
        - VERIFIED인 경우 → "이미 학교 인증이 완료되었습니다."
        - UNVERIFIED인 경우 → "학교 인증 요청이 없습니다."

--- src/main/java/JJinBBang/app/global/security/exception/SecurityAuthException.java
+++ src/main/java/JJinBBang/app/global/security/exception/SecurityAuthException.java
@@ -14,4 +14,21 @@ public class SecurityAuthException extends AuthGroupException {
 	public static SecurityAuthException noAuthentication() {
 		return new SecurityAuthException("인증되지 않은 사용자입니다. 로그인 후 다시 시도하세요.");
 	}
+
+	public static SecurityAuthException alreadyUniversityVerified(){
+		return new SecurityAuthException("이미 학교 인증이 완료되었습니다.");
+	}
+
+	public static SecurityAuthException universityVerificationRequired() {
+		return new SecurityAuthException("학교 인증이 필요합니다.");
+	}
+
+	public static SecurityAuthException existPendingUnivVerifyRequest() {
+		return new SecurityAuthException("대기 중인 학교 인증 요청이 존재합니다.");
+	}
+
+	public static SecurityAuthException pendingUnivVerifyRequestNotFound() {
+		return new SecurityAuthException("학교 인증 요청이 없습니다.");
+	}
+
 }
