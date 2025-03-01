4aad451577e5d0cd65a338be21f6ae9f0f0f15d7
circlezero <wy129@naver.com>
2025-03-02 01:39:57 +0900

    :sparkles: Feat[#13]: Jwt 필터 예외 처리 통일

--- src/main/java/JJinBBang/app/global/jwt/JwtAuthenticationFilter.java
+++ src/main/java/JJinBBang/app/global/jwt/JwtAuthenticationFilter.java
@@ -2,25 +2,30 @@ package JJinBBang.app.global.jwt;
 
 import java.io.IOException;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import JJinBBang.app.domain.user.entity.Users;
 import JJinBBang.app.domain.user.service.UsersService;
 import JJinBBang.app.global.common.enums.VerificationStatus;
+import JJinBBang.app.global.error.exception.NotFoundGroupException;
+import JJinBBang.app.global.jwt.exception.InvalidTokenException;
 import io.jsonwebtoken.Claims;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
@@ -45,9 +50,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
 				// 4. DB에서 유저 정보 조회
 				Users user = usersService.findByProviderId(providerId);
-				if (user == null) {
-					throw new UsernameNotFoundException("해당 사용자를 찾을 수 없습니다.");
-				}
 
 				// 5. SecurityContextHolder에 인증 정보 저장
 				var authentication = new UsernamePasswordAuthenticationToken(user, null, Collections.emptyList());
@@ -56,22 +58,32 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 			// 6. 필터 체인 진행
 			filterChain.doFilter(request, response);
 
-		} catch (Exception e) {
-			// 7. 예외 처리 (필요 시 전역 예외 처리기로 전달 가능)
-			// TODO: 예외 응답 형식 변경해야 함
-			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-			response.setContentType("application/json;charset=UTF-8");
-			response.getWriter().write("{\"error\": \"" + e.getMessage() + "\"}");
+		} catch (InvalidTokenException | NotFoundGroupException e) {
+			makeErrorResponse(e, response);
 		}
 	}
 
+	private void makeErrorResponse(Exception e, HttpServletResponse response) throws IOException {
+		log.error(e.getMessage());
+		response.setContentType("application/json;charset=UTF-8");
+		ObjectMapper mapper = new ObjectMapper();
+		Map<String, Object> errorDetails = new HashMap<>();
 
-	private boolean validateToken(String token) {
-		try {
-			jwtUtils.validateToken(token);
-			return true;
-		} catch (Exception e) {
-			return false;
+		if(e instanceof InvalidTokenException){
+			// 토큰 관련 예외 처리
+			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+			errorDetails.put("code", HttpServletResponse.SC_UNAUTHORIZED);
+			errorDetails.put("message", e.getMessage());
 		}
+		else if(e instanceof NotFoundGroupException){
+			// 유저 관련 예외 처리
+			response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+
+			errorDetails.put("code", HttpServletResponse.SC_NOT_FOUND);
+			errorDetails.put("message", e.getMessage());
+		}
+
+		response.getWriter().write(mapper.writeValueAsString(errorDetails));
 	}
 }
