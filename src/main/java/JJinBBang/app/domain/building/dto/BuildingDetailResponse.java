fb7f30b8434dd26c167e2aac9ae122e8e52ec60e
KJH <fighter0628@gmail.com>
2025-05-15 02:37:39 +0900

    :sparkles: Feat[#52]: BuildingDetailResponse에 빌딩 유형별 DTO 필드와 팩토리 메서드 추가

--- src/main/java/JJinBBang/app/domain/building/dto/BuildingDetailResponse.java
+++ src/main/java/JJinBBang/app/domain/building/dto/BuildingDetailResponse.java
@@ -2,20 +2,49 @@ package JJinBBang.app.domain.building.dto;
 
 import java.util.List;
 
+import JJinBBang.app.domain.building.entity.Agencies;
 import JJinBBang.app.domain.building.entity.Buildings;
+import JJinBBang.app.global.common.dto.AgencyBuildingInfo;
+import JJinBBang.app.global.common.dto.DormitoryBuildingInfo;
+import JJinBBang.app.global.common.dto.GeneralBuildingInfo;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 
 @Builder
 public record BuildingDetailResponse(
-	BasicInfo basicInfo,
-	BuildingImages buildingImages,
-	List<KeywordCount> keywords
+		@JsonInclude(JsonInclude.Include.NON_NULL)
+		GeneralBuildingInfo generalBuildingInfo,
+
+		@JsonInclude(JsonInclude.Include.NON_NULL)
+		DormitoryBuildingInfo dormitoryBuildingInfo,
+
+		@JsonInclude(JsonInclude.Include.NON_NULL)
+		AgencyBuildingInfo agencyBuildingInfo,
+
+		ReviewImages reviewImages,
+		List<KeywordCount> keywords
 ) {
-	public static BuildingDetailResponse of(Buildings building, Boolean liked, List<KeywordCount> keywords) {
+	public static BuildingDetailResponse ofGeneral(Buildings building, Boolean liked, List<KeywordCount> keywords) {
 		return BuildingDetailResponse.builder()
-			.basicInfo(BasicInfo.of(building, liked))
-			.buildingImages(BuildingImages.from(building))
+			.generalBuildingInfo(GeneralBuildingInfo.of(building, liked))
+			.reviewImages(ReviewImages.from(building))
 			.keywords(keywords)
 			.build();
+	}
+
+	public static BuildingDetailResponse ofDormitory(Buildings building, Boolean liked, List<KeywordCount> keywords) {
+		return BuildingDetailResponse.builder()
+				.dormitoryBuildingInfo(DormitoryBuildingInfo.of(building, liked))
+				.reviewImages(ReviewImages.from(building))
+				.keywords(keywords)
+				.build();
+	}
+
+	public static BuildingDetailResponse ofAgency(Agencies agency, Boolean liked, List<KeywordCount> keywords) {
+		return BuildingDetailResponse.builder()
+				.agencyBuildingInfo(AgencyBuildingInfo.of(agency, liked))
+				.reviewImages(ReviewImages.from(agency))
+				.keywords(keywords)
+				.build();
 	}
 }
