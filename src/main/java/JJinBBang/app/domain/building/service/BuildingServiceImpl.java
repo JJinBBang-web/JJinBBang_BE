8ed78579810f0647d0081764bb184aec98fe6707
KJH <fighter0628@gmail.com>
2025-05-15 02:49:25 +0900

    :sparkles: Feat[#52]: 키워드 조회 및 빌딩 타입 분기 로직 추가

--- src/main/java/JJinBBang/app/domain/building/service/BuildingServiceImpl.java
+++ src/main/java/JJinBBang/app/domain/building/service/BuildingServiceImpl.java
@@ -1,37 +1,63 @@
 package JJinBBang.app.domain.building.service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
+import JJinBBang.app.domain.building.document.BuildingKeywordCounts;
+import JJinBBang.app.domain.building.enums.BuildingType;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import JJinBBang.app.domain.building.dto.BuildingDetailResponse;
 import JJinBBang.app.domain.building.dto.KeywordCount;
-import JJinBBang.app.domain.building.entity.BuildingLikes;
 import JJinBBang.app.domain.building.entity.Buildings;
 import JJinBBang.app.domain.building.exception.BuildingNullException;
+import JJinBBang.app.domain.building.repository.BuildingKeywordCountRepository;
 import JJinBBang.app.domain.building.repository.BuildingRepository;
+import JJinBBang.app.domain.user.entity.Users;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class BuildingServiceImpl implements BuildingService{
+public class BuildingServiceImpl implements BuildingService {
 	private final BuildingRepository buildingRepository;
-
+	private final BuildingKeywordCountRepository buildingKeywordCountRepository;
 
 	@Override
 	@Transactional(readOnly = true)
-	public BuildingDetailResponse getBuildingDetail(Long buildingId, Long userId) {
+	public BuildingDetailResponse getBuildingDetail(Long buildingId, Users user) {
+
+		// 1) 건물이 없다면 오류
 		Buildings building = buildingRepository.findById(buildingId).orElseThrow(BuildingNullException::new);
 
-		List<BuildingLikes> buildingLike = building.getBuildingLikes();
-		Boolean liked = buildingLike.stream()
-			.anyMatch(like -> like.getUser().getUserId().equals(userId));
+		// 2) 좋아요 여부
+		Boolean liked = building.getBuildingLikes().stream()
+				.anyMatch(like -> like.getUser().equals(user));
 
-		//TODO 건물에 대한 키워드 상위 5개는 몽고 DB 설정 후 구현
-		List<KeywordCount> keywords = new ArrayList<>();
+		// 3) 키워드 조회 - 없으면 새 엔티티
+		BuildingKeywordCounts buildingKeywordCounts = buildingKeywordCountRepository
+				.findByBuildingIdAndIsAgency(buildingId, false)
+				.orElseGet(() -> BuildingKeywordCounts.of(buildingId));
 
-		return BuildingDetailResponse.of(building, liked, keywords);
+		// 4) keywordCounts 조회 - 없으면 새 리스트
+		List<KeywordCount> keywordCounts = buildingKeywordCounts.getKeywordCounts();
+		if(keywordCounts == null){
+			keywordCounts = new ArrayList<>();
+		}
+
+		List<KeywordCount> top5Positive = keywordCounts.stream()
+				// PO 로 시작하는 키워드만 필터링
+				.filter(kc -> kc.key().toString().startsWith("PO"))
+				// 리뷰 수 기준 내림차순 정렬
+				.sorted(Comparator.comparing(KeywordCount::count).reversed())
+				// 상위 5개만
+				.limit(5)
+				.toList();
+
+		// Dormitory 가 있다면 기숙사 상세 정보 반환
+		if (building.getBuildingType().contains(BuildingType.DORMITORY.name())){
+			return BuildingDetailResponse.ofDormitory(building, liked, top5Positive);
+		} else {
+			return BuildingDetailResponse.ofGeneral(building, liked, top5Positive);
+		}
 	}
 }
