040887b9f74e866b85473bfa1f9596f24e589a11
Seo Jiwon <wldnjstj99@gmail.com>
2025-05-14 19:57:34 +0900

    ✨ feat[#14]: controller, service 통합

--- src/main/java/JJinBBang/app/domain/user/controller/UsersController.java
+++ src/main/java/JJinBBang/app/domain/user/controller/UsersController.java
@@ -1,6 +1,8 @@
 package JJinBBang.app.domain.user.controller;
 
+import JJinBBang.app.domain.user.dto.UserInfoResponseDto;
 import JJinBBang.app.domain.user.dto.response.UserInfoResponse;
+import JJinBBang.app.domain.user.exception.InvalidTokenException;
 import JJinBBang.app.global.template.ResTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -15,16 +17,20 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/v1/user/test")
+@RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
 public class UsersController {
 
 	private final UsersService usersService;
 
-	@GetMapping("")
-	public ResTemplate<UserInfoResponse> getUserInfo(@AuthenticationPrincipal Users user) {
+	@GetMapping
+	public ResTemplate<UserInfoResponseDto> getUserInfo(@AuthenticationPrincipal Users user) {
+		if (user == null) {
+			throw InvalidTokenException.unauthorized();
+		}
 		log.info("유저 조회 성공 : {}", user.getUserId());
-		UserInfoResponse response = UserInfoResponse.from(user);
-		return new ResTemplate<>(HttpStatus.OK, "유저 정보 조회 성공", response);
+
+		UserInfoResponseDto response = usersService.getUserInfo(user);
+		return new ResTemplate<>(HttpStatus.OK, "유저 정보조회 성공", response);
 	}
 }
--- src/main/java/JJinBBang/app/domain/user/service/UsersServiceImpl.java
+++ src/main/java/JJinBBang/app/domain/user/service/UsersServiceImpl.java
@@ -1,5 +1,6 @@
 package JJinBBang.app.domain.user.service;
 
+import JJinBBang.app.domain.user.dto.UserInfoResponseDto;
 import org.springframework.stereotype.Service;
 
 import JJinBBang.app.domain.user.entity.Users;
@@ -7,8 +8,6 @@ import JJinBBang.app.domain.user.exception.UserNotFoundException;
 import JJinBBang.app.domain.user.repository.UsersRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
-import java.util.Optional;
 
 @Slf4j
 @Service
@@ -37,5 +36,13 @@ public class UsersServiceImpl implements UsersService {
 	@Override
 	public Users save(Users user) {
 		return usersRepository.save(user);
+	}
+
+	@Override
+	public UserInfoResponseDto getUserInfo(Users user) {
+		if (user == null) {
+			throw UserNotFoundException.notFound();
+		}
+		return UserInfoResponseDto.of(user);
 	}
 }
