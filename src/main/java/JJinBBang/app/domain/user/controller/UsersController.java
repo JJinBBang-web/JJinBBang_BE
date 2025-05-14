abf6715268f785f1ab6cca74b08d1f109fbd20e9
Seo Jiwon <wldnjstj99@gmail.com>
2025-05-14 10:37:45 +0900

    ✨ feat[#14]: user 테스트 url 중복으로 수정

--- src/main/java/JJinBBang/app/domain/user/controller/UsersController.java
+++ src/main/java/JJinBBang/app/domain/user/controller/UsersController.java
@@ -15,7 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/v1/user")
+@RequestMapping("/api/v1/user/test")
 @RequiredArgsConstructor
 public class UsersController {
 
