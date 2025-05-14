ce622b39cc238df38306fef0d3ce66dfafed2b40
KJH <fighter0628@gmail.com>
2025-05-15 02:54:26 +0900

    :sparkles: Feat[#52]: BuildingController에 공인중개사 추가 구현

--- src/main/java/JJinBBang/app/domain/building/controller/BuildingController.java
+++ src/main/java/JJinBBang/app/domain/building/controller/BuildingController.java
@@ -1,13 +1,14 @@
 package JJinBBang.app.domain.building.controller;
 
+import JJinBBang.app.domain.building.service.AgencyService;
+import lombok.NonNull;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 import JJinBBang.app.domain.building.dto.BuildingDetailResponse;
 import JJinBBang.app.domain.building.service.BuildingService;
+import JJinBBang.app.domain.user.entity.Users;
 import JJinBBang.app.global.template.ResTemplate;
 import lombok.RequiredArgsConstructor;
 
@@ -16,13 +17,21 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class BuildingController {
 	private final BuildingService buildingService;
+	private final AgencyService agencyService;
 
 	@GetMapping("/{buildingId}")
-	public ResTemplate<BuildingDetailResponse> getBuildingDetail(@PathVariable Long buildingId) {
-		//TODO 토큰에서 사용자 정보 얻는 로직 개발 전까지 하드코딩 사용
-		Long userId = 1L;
+	public ResTemplate<BuildingDetailResponse> getBuildingDetail(
+			@PathVariable Long buildingId,
+			@NonNull @RequestParam Boolean isAgency,
+			@AuthenticationPrincipal Users user
+			) {
 
-		BuildingDetailResponse data = buildingService.getBuildingDetail(buildingId, userId);
+		BuildingDetailResponse data;
+		if (isAgency) {
+			data = agencyService.getAgencyDetail(buildingId, user);
+        } else {
+			data = buildingService.getBuildingDetail(buildingId, user);
+		}
 		return new ResTemplate<>(HttpStatus.OK, "건물 상세 불러오기 성공", data);
 	}
 }
