1e54c51ee0ae11b6175d8e323baaebffeae7c3b2
KJH <fighter0628@gmail.com>
2025-05-14 21:15:38 +0900

    :sparkles: Feat[#52]: 컴포넌트 이름 변경으로 인한 클래스 이름 변경

--- src/main/java/JJinBBang/app/domain/building/dto/ReviewImages.java
+++ src/main/java/JJinBBang/app/domain/building/dto/ReviewImages.java
@@ -2,19 +2,27 @@ package JJinBBang.app.domain.building.dto;
 
 import java.util.List;
 
+import JJinBBang.app.domain.building.entity.Agencies;
 import JJinBBang.app.domain.building.entity.Buildings;
 import JJinBBang.app.domain.building.entity.Reviews;
 import lombok.Builder;
 
 @Builder
-public record BuildingImages(
+public record ReviewImages(
 	Integer count,
 	List<String> imageUrl
 ) {
-	public static BuildingImages from(Buildings building) {
-		return BuildingImages.builder()
+	public static ReviewImages from(Buildings building) {
+		return ReviewImages.builder()
 			.count(building.getImagesCount())
 			.imageUrl(building.getReviews().stream().map(Reviews::getThumbnailImage).toList())
 			.build();
+	}
+
+	public static ReviewImages from(Agencies agency) {
+		return ReviewImages.builder()
+				.count(agency.getImagesCount())
+				.imageUrl(agency.getReviews().stream().map(Reviews::getThumbnailImage).toList())
+				.build();
 	}
 }
