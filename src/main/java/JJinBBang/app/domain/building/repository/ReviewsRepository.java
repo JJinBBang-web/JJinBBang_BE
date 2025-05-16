09726067b881e462c5b7d49ba00a106f95216da0
이루경 <dlfnrud0605@gmail.com>
2025-05-16 21:22:17 +0900

    🐛 Fix[#26]: 레포지토리 매개변수 변경 String->ReviewType

--- src/main/java/JJinBBang/app/domain/building/repository/ReviewsRepository.java
+++ src/main/java/JJinBBang/app/domain/building/repository/ReviewsRepository.java
@@ -3,9 +3,10 @@ package JJinBBang.app.domain.building.repository;
 import JJinBBang.app.domain.building.entity.Agencies;
 import JJinBBang.app.domain.building.entity.Buildings;
 import JJinBBang.app.domain.building.entity.Reviews;
+import JJinBBang.app.domain.building.enums.ReviewType;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ReviewsRepository extends CrudRepository<Reviews,Long> {
     Reviews findFirstByBuildingOrderByCreatedAtDesc(Buildings building);
-    Reviews findFirstByAgencyAndDtypeOrderByCreatedAtDesc(Agencies agency, String reviewType);
+    Reviews findFirstByAgencyAndDtypeOrderByCreatedAtDesc(Agencies agency, ReviewType dtype);
 }
