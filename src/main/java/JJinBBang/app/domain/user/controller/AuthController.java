58a0c7d5c3c56530fb69e45b7c77e2546032e67f
circlezero <wy129@naver.com>
2025-05-18 04:22:43 +0900

    :sparkles: Feat[#63] 이메일 인증코드 발급, 검증 API 개발

--- src/main/java/JJinBBang/app/domain/user/controller/AuthController.java
+++ src/main/java/JJinBBang/app/domain/user/controller/AuthController.java
@@ -1,12 +1,15 @@
 package JJinBBang.app.domain.user.controller;
 
+import JJinBBang.app.domain.user.dto.request.IssueEmailCodeRequest;
 import JJinBBang.app.domain.user.dto.request.LoginRequest;
 import JJinBBang.app.domain.user.dto.request.SignupRequest;
 import JJinBBang.app.domain.user.dto.response.LoginResponse;
 import JJinBBang.app.domain.user.dto.response.SignupRequiredResponse;
 import JJinBBang.app.domain.user.entity.Users;
 import JJinBBang.app.domain.user.service.OAuthService;
+import JJinBBang.app.domain.user.service.UsersService;
 import JJinBBang.app.global.jwt.JwtUtils;
+import JJinBBang.app.global.mail.service.MailAuthService;
 import JJinBBang.app.global.template.ResTemplate;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,19 +26,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
 
+    private final UsersService usersService;
     private final OAuthService oAuthService;
     private final JwtUtils jwtUtils;
+    private final MailAuthService mailAuthService;
 
     @PostMapping
-    public ResTemplate<?> signIn(@RequestBody LoginRequest loginRequest){
+    public ResTemplate<?> signIn(@RequestBody LoginRequest loginRequest) {
         Users user = oAuthService.login(loginRequest.oauthProvider(), loginRequest.oauthCode());
-        if(user.getUserId() == null){
+        if (user.getUserId() == null) {
             String signupToken = jwtUtils.generateSignupToken(user);
 
             SignupRequiredResponse signupRequiredResponse = SignupRequiredResponse.of(signupToken);
             return new ResTemplate<>(HttpStatus.OK, "약관동의가 필요합니다.", signupRequiredResponse);
-        }
-        else {
+        } else {
             String accessToken = jwtUtils.generateAccessToken(user);
             String refreshToken = jwtUtils.generateRefreshToken(user);
 
@@ -45,12 +49,41 @@ public class AuthController {
     }
 
     @PostMapping("/signup")
-    public ResTemplate<LoginResponse> signUp(@AuthenticationPrincipal Users user){
+    public ResTemplate<LoginResponse> signUp(@AuthenticationPrincipal Users user) {
         user = oAuthService.signup(user);
         String accessToken = jwtUtils.generateAccessToken(user);
         String refreshToken = jwtUtils.generateRefreshToken(user);
 
         LoginResponse loginResponse = LoginResponse.of(accessToken, refreshToken);
         return new ResTemplate<>(HttpStatus.OK, "회원가입 성공", loginResponse);
+    }
+
+    @PostMapping("/emailCode")
+    public ResTemplate<?> sendEmailCode(
+            @AuthenticationPrincipal Users user,
+            @RequestBody IssueEmailCodeRequest request
+    ) {
+        String email = request.emailAddress();
+        mailAuthService.sendAuthCode(email);
+        return new ResTemplate<>(HttpStatus.OK, "인증코드 전송 완료", null);
+    }
+
+    @PostMapping("/emailCode/verify")
+    public ResTemplate<?> verifyEmailCode(
+            @AuthenticationPrincipal Users user,
+            @RequestBody IssueEmailCodeRequest request
+    ) {
+        String email = request.emailAddress();
+        String code = request.emailAddress();
+
+        // 인증코드 검증
+        // 인증코드 만료 또는 미발급은 예외 반환
+        boolean verifyResult = mailAuthService.verifyAuthCode(email, code);
+        if(!verifyResult) {
+            usersService.verifyUniversityEmail(user, email);
+            return new ResTemplate<>(HttpStatus.OK, "인증이 완료되었습니다.", null);
+        } else {
+            return new ResTemplate<>(HttpStatus.BAD_REQUEST, "인증코드 검증에 실패하였습니다.", null);
+        }
     }
 }
