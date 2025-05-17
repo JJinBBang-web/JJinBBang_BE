d73543bf530827b8001f70ef8b37becf3943912c
KJH <fighter0628@gmail.com>
2025-05-18 02:11:59 +0900

    ✨ Feat[#60]: Building DTO 추가 및 of 메서드 구현

---
+++ src/main/java/JJinBBang/app/domain/building/dto/Building.java
@@ -0,0 +1,38 @@
+package JJinBBang.app.domain.building.dto;
+
+import JJinBBang.app.domain.building.entity.Agencies;
+import JJinBBang.app.domain.building.entity.Buildings;
+import JJinBBang.app.domain.building.enums.BuildingType;
+import lombok.Builder;
+
+@Builder
+public record Building (
+	Long buildingId,
+	String name,
+	BuildingType type,
+	String address,
+	Double latitude,
+	Double longitude
+){
+	public static Building of(Buildings building, BuildingType buildingType) {
+		return Building.builder()
+			.buildingId(building.getId())
+			.name(building.getBuildingName())
+			.type(buildingType)
+			.address(building.getBuildingAddress())
+			.latitude(building.getBuildingLat())
+			.longitude(building.getBuildingLat())
+			.build();
+	}
+
+	public static Building of(Agencies agency, BuildingType buildingType) {
+		return Building.builder()
+			.buildingId(agency.getAgencyId())
+			.name(agency.getName())
+			.type(buildingType)
+			.address(agency.getAddress())
+			.latitude(agency.getAgencyLat())
+			.longitude(agency.getAgencyLat())
+			.build();
+	}
+}
