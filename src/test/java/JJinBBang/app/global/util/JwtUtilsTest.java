b322a18e19a20d18a681e2ff393cbe499996f1ff
circlezero <wy129@naver.com>
2025-05-18 10:28:16 +0900

    :white_check_mark: Test[#66] 테스트 로직 수정

--- src/test/java/JJinBBang/app/global/util/JwtUtilsTest.java
+++ src/test/java/JJinBBang/app/global/util/JwtUtilsTest.java
@@ -8,10 +8,11 @@ import JJinBBang.app.global.common.enums.Provider;
 import JJinBBang.app.global.jwt.JwtUtils;
 import JJinBBang.app.global.jwt.exception.InvalidTokenException;
 import JJinBBang.app.global.jwt.repository.InMemoryRefreshTokenRepository;
-import JJinBBang.app.global.jwt.service.TokenService;
-import JJinBBang.app.global.jwt.service.impl.AccessTokenServiceImpl;
-import JJinBBang.app.global.jwt.service.impl.RefreshTokenServiceImpl;
-import JJinBBang.app.global.jwt.service.impl.SignupTokenServiceImpl;
+import JJinBBang.app.global.jwt.service.RefreshTokenService;
+import JJinBBang.app.global.jwt.service.TokenGenerateService;
+import JJinBBang.app.global.jwt.service.impl.AccessTokenGenerateServiceImpl;
+import JJinBBang.app.global.jwt.service.impl.RefreshTokenGenerateServiceImpl;
+import JJinBBang.app.global.jwt.service.impl.SignupTokenGenerateServiceImpl;
 import io.jsonwebtoken.Claims;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -29,9 +30,14 @@ class JwtUtilsTest {
 	private static final String SECRET_KEY =
 			"MDEyMzQ1Njc4OTAxMjM0NTY3ODkwMTIzNDU2Nzg5MDE=";
 
-	@Mock TokenService accessTokenService;
-	@Mock TokenService refreshTokenService;
-	@Mock TokenService signupTokenService;
+	@Mock
+	TokenGenerateService accessTokenGenerateService;
+	@Mock
+	TokenGenerateService refreshTokenGenerateService;
+	@Mock
+	TokenGenerateService signupTokenGenerateService;
+	@Mock
+	RefreshTokenService refreshTokenService;
 
 	JwtUtils jwtUtils;  // Mockito가 생성자 인젝션 해 줌
 
@@ -42,9 +48,10 @@ class JwtUtilsTest {
 		// 생성자에 SECRET_KEY와 mock TokenService들을 주입
 		jwtUtils = new JwtUtils(
 				SECRET_KEY,
-				accessTokenService,
-				refreshTokenService,
-				signupTokenService
+				accessTokenGenerateService,
+				refreshTokenGenerateService,
+				signupTokenGenerateService,
+				refreshTokenService
 		);
 
 		// DB 호출 없이 builder만 사용
@@ -58,35 +65,35 @@ class JwtUtilsTest {
 
 	@Test
 	void generateAccessToken_위임() {
-		when(accessTokenService.generateToken(testUser))
+		when(accessTokenGenerateService.generateToken(testUser))
 				.thenReturn("ACCESS_TOKEN");
 
 		String token = jwtUtils.generateAccessToken(testUser);
 
 		assertEquals("ACCESS_TOKEN", token);
-		verify(accessTokenService).generateToken(testUser);
+		verify(accessTokenGenerateService).generateToken(testUser);
 	}
 
 	@Test
 	void generateRefreshToken_위임() {
-		when(refreshTokenService.generateToken(testUser))
+		when(refreshTokenGenerateService.generateToken(testUser))
 				.thenReturn("REFRESH_TOKEN");
 
 		String token = jwtUtils.generateRefreshToken(testUser);
 
 		assertEquals("REFRESH_TOKEN", token);
-		verify(refreshTokenService).generateToken(testUser);
+		verify(refreshTokenGenerateService).generateToken(testUser);
 	}
 
 	@Test
 	void generateSignupToken_위임() {
-		when(signupTokenService.generateToken(testUser))
+		when(signupTokenGenerateService.generateToken(testUser))
 				.thenReturn("SIGNUP_TOKEN");
 
 		String token = jwtUtils.generateSignupToken(testUser);
 
 		assertEquals("SIGNUP_TOKEN", token);
-		verify(signupTokenService).generateToken(testUser);
+		verify(signupTokenGenerateService).generateToken(testUser);
 	}
 
 	@Test
@@ -110,17 +117,18 @@ class JwtUtilsTest {
 	@Test
 	void realToken_parseClaimsAndGetProviderAndStatus() {
 		// 1) 실제 TokenService 구현체 생성
-		var accessSvc  = new AccessTokenServiceImpl(SECRET_KEY, 60_000L);
-		var refreshSvc = new RefreshTokenServiceImpl(
+		var accessSvc  = new AccessTokenGenerateServiceImpl(SECRET_KEY, 60_000L);
+		var refreshSvc = new RefreshTokenGenerateServiceImpl(
 				SECRET_KEY,
 				60_000L,
 				new InMemoryRefreshTokenRepository()
 		);
-		var signupSvc  = new SignupTokenServiceImpl(SECRET_KEY, 60_000L);
+		var signupSvc  = new SignupTokenGenerateServiceImpl(SECRET_KEY, 60_000L);
+		var refreshTokenSvc = mock(RefreshTokenService.class);
 
 		// 2) JwtUtils 인스턴스 생성
 		var realJwtUtils = new JwtUtils(
-				SECRET_KEY, accessSvc, refreshSvc, signupSvc
+				SECRET_KEY, accessSvc, refreshSvc, signupSvc, refreshTokenSvc
 		);
 
 		// 3) 테스트용 Users
@@ -152,15 +160,17 @@ class JwtUtilsTest {
 	@Test
 	void extractAllClaimsAsMap_ReturnsCorrectMap() {
 		// real instance 재사용
-		var accessSvc  = new AccessTokenServiceImpl(SECRET_KEY, 60_000L);
-		var refreshSvc = new RefreshTokenServiceImpl(
+		var accessSvc  = new AccessTokenGenerateServiceImpl(SECRET_KEY, 60_000L);
+		var refreshSvc = new RefreshTokenGenerateServiceImpl(
 				SECRET_KEY,
 				60_000L,
 				new InMemoryRefreshTokenRepository()
 		);
-		var signupSvc  = new SignupTokenServiceImpl(SECRET_KEY, 60_000L);
+		var signupSvc  = new SignupTokenGenerateServiceImpl(SECRET_KEY, 60_000L);
+		var refreshTokenSvc = mock(RefreshTokenService.class);
+
 		var realJwtUtils = new JwtUtils(
-				SECRET_KEY, accessSvc, refreshSvc, signupSvc
+				SECRET_KEY, accessSvc, refreshSvc, signupSvc, refreshTokenSvc
 		);
 
 		Users user = Users.builder()
@@ -183,15 +193,17 @@ class JwtUtilsTest {
 	@Test
 	void validateToken_expired_Throws() throws InterruptedException {
 		// 1초 만료 서비스
-		var accessSvc  = new AccessTokenServiceImpl(SECRET_KEY, 1_000L);
-		var refreshSvc = new RefreshTokenServiceImpl(
+		var accessSvc  = new AccessTokenGenerateServiceImpl(SECRET_KEY, 1_000L);
+		var refreshSvc = new RefreshTokenGenerateServiceImpl(
 				SECRET_KEY,
 				1_000L,
 				new InMemoryRefreshTokenRepository()
 		);
-		var signupSvc  = new SignupTokenServiceImpl(SECRET_KEY, 1_000L);
+		var signupSvc  = new SignupTokenGenerateServiceImpl(SECRET_KEY, 1_000L);
+		var refreshTokenSvc = mock(RefreshTokenService.class);
+
 		var shortJwt   = new JwtUtils(
-				SECRET_KEY, accessSvc, refreshSvc, signupSvc
+				SECRET_KEY, accessSvc, refreshSvc, signupSvc, refreshTokenSvc
 		);
 
 		Users user = Users.builder()
