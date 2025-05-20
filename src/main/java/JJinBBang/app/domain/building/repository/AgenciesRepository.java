424cf70e390dfe72f3fd02f91c546018cb5eb4b7
KJH <fighter0628@gmail.com>
2025-05-20 16:38:53 +0900

    :wrench: Chore[#62]: AgencyRepository, AgenciesRepository 를 AgenciesRepository 로 통합

--- src/main/java/JJinBBang/app/domain/building/repository/AgenciesRepository.java
+++ src/main/java/JJinBBang/app/domain/building/repository/AgenciesRepository.java
@@ -3,5 +3,8 @@ package JJinBBang.app.domain.building.repository;
 import JJinBBang.app.domain.building.entity.Agencies;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AgenciesRepository extends JpaRepository<Agencies, Long> {
+    Optional<Agencies> findByBuildingCode(String buildingCode);
 }
