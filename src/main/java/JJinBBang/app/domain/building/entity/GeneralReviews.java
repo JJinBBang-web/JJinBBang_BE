022a056bb7973a48efa967410e80c0cf5d7829c8
KJH <fighter0628@gmail.com>
2025-05-20 16:46:04 +0900

    ✨ Feat[#62]: Reviews 자식 엔티티에 Builder 및 생성자 추가

--- src/main/java/JJinBBang/app/domain/building/entity/GeneralReviews.java
+++ src/main/java/JJinBBang/app/domain/building/entity/GeneralReviews.java
@@ -1,12 +1,23 @@
 package JJinBBang.app.domain.building.entity;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import JJinBBang.app.domain.building.enums.ContractType;
 import JJinBBang.app.domain.building.enums.Floor;
+import JJinBBang.app.domain.building.enums.ReviewType;
+import JJinBBang.app.domain.user.entity.Users;
+import JJinBBang.app.global.common.enums.KeywordType;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "general_reviews")
 @PrimaryKeyJoinColumn(name = "review_id")
 @DiscriminatorValue("GENERAL")
@@ -30,4 +41,33 @@ public class GeneralReviews extends Reviews {
 
 	@Column(name = "maintenance_cost", nullable = true)
 	private Integer maintenanceCost;
+
+	@Builder
+	private GeneralReviews(Long id,
+		ReviewType dtype,
+		Integer likesCount,
+		String thumbnailImage,
+		String content,
+		List<KeywordType> tags,
+		BigDecimal rating,
+		Users user,
+		Buildings building,
+		Agencies agency,
+		List<ReviewLikes> reviewLikes,
+
+		Floor floor,
+		Double area,
+		ContractType contractType,
+		Integer deposit,
+		Integer price,
+		Integer maintenanceCost
+	) {
+		super(id, dtype, likesCount, thumbnailImage, content, tags, rating, user, building, agency, reviewLikes);
+		this.floor           = floor;
+		this.area            = area;
+		this.contractType    = contractType;
+		this.deposit         = deposit;
+		this.price           = price;
+		this.maintenanceCost = maintenanceCost;
+	}
 }
