3566bc585e61e8d5c1d4d391a3eedee7b40ff1c8
KJH <fighter0628@gmail.com>
2025-05-20 16:35:50 +0900

    ✨ Feat[#62]: Agencies 엔티티에 Builder, buildingCode 필드 및 유틸 메서드 추가

--- src/main/java/JJinBBang/app/domain/building/entity/Agencies.java
+++ src/main/java/JJinBBang/app/domain/building/entity/Agencies.java
@@ -3,15 +3,20 @@ package JJinBBang.app.domain.building.entity;
 import JJinBBang.app.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Getter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Agencies extends BaseEntity {
 
@@ -22,6 +27,9 @@ public class Agencies extends BaseEntity {
 
 	@Column(name = "agency_serial")
 	private Long agencySerial;
+
+	@Column(name = "building_code", nullable = false, unique = true)
+	private String buildingCode; // 카카오 건물 관리 번호
 
 	@Column(name = "name", length = 255, nullable = false)
 	private String name;
@@ -35,7 +43,7 @@ public class Agencies extends BaseEntity {
 	@Column(name = "agency_lot", nullable = false)
 	private Double agencyLot;
 
-	@Column(name = "rating", precision = 3, scale = 2, nullable = false)
+	@Column(name = "rating", precision = 3, scale = 2, nullable = true)
 	private BigDecimal rating;
 
 	@Column(name = "like_count", nullable = false)
@@ -49,8 +57,35 @@ public class Agencies extends BaseEntity {
 
 	// 공인중개사 -> 공인중개사-좋아요
 	@OneToMany(mappedBy = "agency", cascade = CascadeType.ALL)
+	@Builder.Default
 	private List<AgencyLikes> agencyLikes = new ArrayList<>();
 
 	@OneToMany(mappedBy = "agency", cascade = CascadeType.ALL)
+	@Builder.Default
 	private List<Reviews> reviews = new ArrayList<>();
+
+	public void incrementReviewCount() {
+		this.reviewCount++;
+	}
+
+	public void updateAverageRating(BigDecimal newRating) {
+		if (this.rating == null) {
+			this.rating = newRating;
+			return;
+		}
+
+		int count = this.reviewCount;
+
+		BigDecimal previousTotal = this.rating
+			.multiply(BigDecimal.valueOf(count - 1));
+
+		BigDecimal newTotal = previousTotal.add(newRating);
+
+		this.rating = newTotal
+			.divide(BigDecimal.valueOf(count), 2, RoundingMode.HALF_UP);
+	}
+
+	public void incrementImagesCount() {
+		this.imagesCount++;
+	}
 }
