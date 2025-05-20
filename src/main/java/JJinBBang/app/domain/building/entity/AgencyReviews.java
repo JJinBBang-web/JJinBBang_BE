e808ba86acbc769d9f8127db181fdf0fb42b7c56
KJH <fighter0628@gmail.com>
2025-05-20 16:46:04 +0900

    ✨ Feat[#62]: Reviews 자식 엔티티에 Builder 및 생성자 추가

--- src/main/java/JJinBBang/app/domain/building/entity/AgencyReviews.java
+++ src/main/java/JJinBBang/app/domain/building/entity/AgencyReviews.java
@@ -1,10 +1,35 @@
 package JJinBBang.app.domain.building.entity;
 
+import java.math.BigDecimal;
+import java.util.List;
+
+import JJinBBang.app.domain.building.enums.ReviewType;
+import JJinBBang.app.domain.user.entity.Users;
+import JJinBBang.app.global.common.enums.KeywordType;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 
 @Entity
+@AllArgsConstructor
 @Table(name = "agency_reviews")
 @PrimaryKeyJoinColumn(name = "review_id")
 @DiscriminatorValue("AGENCY")
 public class AgencyReviews extends Reviews {
+	@Builder
+	private AgencyReviews(
+		Long id,
+		ReviewType dtype,
+		Integer likesCount,
+		String thumbnailImage,
+		String content,
+		List<KeywordType> tags,
+		BigDecimal rating,
+		Users user,
+		Buildings building,
+		Agencies agency,
+		List<ReviewLikes> reviewLikes
+	) {
+		super(id, dtype, likesCount, thumbnailImage, content, tags, rating, user, building, agency, reviewLikes);
+	}
 }
--- src/main/java/JJinBBang/app/domain/building/entity/DormReviews.java
+++ src/main/java/JJinBBang/app/domain/building/entity/DormReviews.java
@@ -1,14 +1,21 @@
 package JJinBBang.app.domain.building.entity;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
 import JJinBBang.app.domain.building.enums.Floor;
+import JJinBBang.app.domain.building.enums.ReviewType;
+import JJinBBang.app.domain.user.entity.Users;
+import JJinBBang.app.global.common.enums.KeywordType;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor
 @Table(name = "dorm_reviews")
 @PrimaryKeyJoinColumn(name = "review_id")
 @DiscriminatorValue("DORM")
@@ -34,4 +41,33 @@ public class DormReviews extends Reviews {
 	@OneToMany(mappedBy = "dormitoryReview", cascade = CascadeType.ALL)
 	private List<DormitoryFacilities> dormitoryFacilities = new ArrayList<>();
 
+	@Builder
+	private DormReviews(
+		Long id,
+		Integer likesCount,
+		ReviewType dtype,
+		String thumbnailImage,
+		String content,
+		List<KeywordType> tags,
+		BigDecimal rating,
+		Users user,
+		Buildings building,
+		Agencies agency,
+		List<ReviewLikes> reviewLikes,
+
+		Floor floor,
+		Integer capacity,
+		Integer dormFee,
+		String currentRegion,
+		Double currentGrade,
+		List<DormitoryFacilities> dormitoryFacilities
+	) {
+		super(id, dtype, likesCount, thumbnailImage, content, tags, rating, user, building, agency, reviewLikes);
+		this.floor = floor;
+		this.capacity = capacity;
+		this.dormFee = dormFee;
+		this.currentRegion = currentRegion;
+		this.currentGrade = currentGrade;
+		this.dormitoryFacilities = dormitoryFacilities;
+	}
 }
