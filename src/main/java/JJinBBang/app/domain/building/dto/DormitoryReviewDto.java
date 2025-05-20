a4ed9c9dc11f422ae43b57e43521d7cf6ffe6af0
KJH <fighter0628@gmail.com>
2025-05-20 17:06:08 +0900

    ✨ Feat[#62]: 리뷰 요청용 DTO(DormitoryReviewDto, GeneralReviewDto, ReviewRequest) 생성

---
+++ src/main/java/JJinBBang/app/domain/building/dto/DormitoryReviewDto.java
@@ -0,0 +1,27 @@
+package JJinBBang.app.domain.building.dto;
+
+import java.math.BigDecimal;
+
+import JJinBBang.app.domain.building.enums.Floor;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
+public class DormitoryReviewDto {
+	@NotBlank
+	private String campus;
+	@NotNull
+	@Min(1)
+	private Integer capacity;
+	@NotNull @Min(0)
+	private Integer dormFee;
+	@NotNull
+	private Floor floor;
+	@NotNull @Min(1) @Max(5)
+	private BigDecimal rating;
+	@NotBlank
+	private String content;
+}
