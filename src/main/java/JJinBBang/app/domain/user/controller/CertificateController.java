b2fbec258b6f070f5b8156ab3733167e4bc383f6
Seo Jiwon <wldnjstj99@gmail.com>
2025-05-22 10:36:51 +0900

    ✨ feat[#56]: 합격증명서, 재학증명서 업로드 API 작성

--- src/main/java/JJinBBang/app/domain/user/controller/CertificateController.java
+++ src/main/java/JJinBBang/app/domain/user/controller/CertificateController.java
@@ -1,62 +1,76 @@
 package JJinBBang.app.domain.user.controller;
 
-import JJinBBang.app.domain.user.dto.request.CertificateRequest;
-import JJinBBang.app.domain.user.service.CertificateDriveService;
-import JJinBBang.app.domain.user.service.CertificateSheetsService;
+import JJinBBang.app.domain.user.entity.Users;
+import JJinBBang.app.domain.user.service.CertificateService;
+import JJinBBang.app.domain.user.service.UsersService;
 import JJinBBang.app.global.template.ResTemplate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.io.IOException;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/v1/certificates")
 @RequiredArgsConstructor
 public class CertificateController {
 
-    private final CertificateDriveService certificateDriveService;
-    private final CertificateSheetsService certificateSheetsService;
+    private final CertificateService certificateService;
+    private final UsersService usersService;
 
-    @PostMapping(value = "/enrollment/verify", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResTemplate<?> uploadEnrollmentCertificate(@ModelAttribute CertificateRequest certificateRequest) throws IOException {
-        String sharedLink = certificateDriveService.uploadPdf(certificateRequest.certificate());
+    // 재학증명서 업로드
+    @PostMapping("/enrollment/verify")
+    public ResTemplate<?> uploadEnrollmentCertificate(
+            @AuthenticationPrincipal Users principal,
+            @RequestPart("file") MultipartFile file
+    ) {
+        Users user = usersService.findWithUniversity(principal.getProviderId());
 
-        certificateSheetsService.appendCertificateSheet(
-                "jb_enrollment_verify_sheet", // 시트 이름 (수정 필요)
-                certificateRequest.universityId(),
-                certificateRequest.studentNumber(),
-                certificateRequest.certificate().getOriginalFilename(),
-                sharedLink
+        // 대학교 이름을 기준으로 폴더 지정
+        String folderName = user.getUniversity().getUniversityName();
+
+        // 구글 드라이브에 업로드 및 링크 반환
+        String fileLink = certificateService.uploadEnrollmentFileToDrive(file, folderName);
+
+        // 스프레드 시트에 row로 업로드 (userId, universityId, 파일링크, 업로드 시간)
+        certificateService.appendEnrollmentFileToSheets(
+                user.getUserId().intValue(),
+                user.getUniversity().getId().intValue(),
+                file.getOriginalFilename(),
+                fileLink
         );
 
-        return new ResTemplate<>(
-                HttpStatus.OK,
+        return new ResTemplate<>(HttpStatus.OK,
                 "업로드 성공",
-                sharedLink
-        );
+                null);
     }
 
-    @PostMapping(value = "/admission/verify", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResTemplate<?> uploadAdmissionCertificate(@ModelAttribute CertificateRequest certificateRequest) throws IOException {
-        String sharedLink = certificateDriveService.uploadPdf(certificateRequest.certificate());
+    // 합격증명서 업로드
+    @PostMapping("/admission/verify")
+    public ResTemplate<?> uploadAdmissionCertificate(
+            @AuthenticationPrincipal Users principal,
+            @RequestPart("file") MultipartFile file
+    ) {
+        Users user = usersService.findWithUniversity(principal.getProviderId());
 
-        certificateSheetsService.appendCertificateSheet(
-                "jb_admission_verify_sheet", // 시트 이름 (수정 필요)
-                certificateRequest.universityId(),
-                certificateRequest.studentNumber(),
-                certificateRequest.certificate().getOriginalFilename(),
-                sharedLink
+        // 구글 드라이브에 업로드 및 링크 반환
+        String folderName = user.getUniversity().getUniversityName();
+
+        // 구글 드라이브에 업로드 및 링크 반환
+        String fileLink = certificateService.uploadAdmissionFileToDrive(file, folderName);
+
+        // 스프레드 시트에 row로 업로드 (userId, universityId, 파일링크, 업로드 시간)
+        certificateService.appendAdmissionFileToSheets(
+                user.getUserId().intValue(),
+                user.getUniversity().getId().intValue(),
+                file.getOriginalFilename(),
+                fileLink
         );
 
         return new ResTemplate<>(
                 HttpStatus.OK,
                 "업로드 성공",
-                sharedLink
+                null
         );
     }
 }
