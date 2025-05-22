a2b23b517d8f195510db180361800ac9f74734c9
Seo Jiwon <wldnjstj99@gmail.com>
2025-05-22 10:27:13 +0900

    ✨ feat[#56]: google api iam 키 추가 및 api 호출 설정

--- src/main/java/JJinBBang/app/global/config/GoogleApiConfig.java
+++ src/main/java/JJinBBang/app/global/config/GoogleApiConfig.java
@@ -1,7 +1,5 @@
 package JJinBBang.app.global.config;
 
-import JJinBBang.app.domain.user.service.CertificateDriveService;
-import JJinBBang.app.domain.user.service.CertificateSheetsService;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
@@ -11,67 +9,56 @@ import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.SheetsScopes;
 import com.google.auth.http.HttpCredentialsAdapter;
 import com.google.auth.oauth2.ServiceAccountCredentials;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
-import java.io.IOException;
-import java.security.GeneralSecurityException;
 import java.util.List;
 
 @Configuration
 public class GoogleApiConfig {
-    @Value("${google.drive.folder-id}")
-    private String driveFolderId;
 
-    @Value("${google.spreadsheet.id}")
-    private String spreadsheetId;
+    private final GoogleProperties props;
+
+    public GoogleApiConfig(GoogleProperties props) {
+        this.props = props;
+    }
 
     @Bean
-    public Drive googleDrive() throws IOException, GeneralSecurityException {
+    public Drive googleDrive() throws Exception {
         JsonFactory jsonFactory = GsonFactory.getDefaultInstance();
 
-        var credentials = ServiceAccountCredentials.fromStream(
-                new ClassPathResource("google.json").getInputStream()
-        ).createScoped(List.of(
-                DriveScopes.DRIVE_FILE,
-                DriveScopes.DRIVE_METADATA
-        ));
+        var creds = ServiceAccountCredentials
+                .fromStream(new ClassPathResource("gcp_iam_key.json").getInputStream())
+                .createScoped(List.of(
+                        DriveScopes.DRIVE_FILE,
+                        DriveScopes.DRIVE_METADATA
+                ));
 
         return new Drive.Builder(
                 GoogleNetHttpTransport.newTrustedTransport(),
                 jsonFactory,
-                new HttpCredentialsAdapter(credentials)
+                new HttpCredentialsAdapter(creds)
         )
-                .setApplicationName("CertificateDrive")
+                .setApplicationName("JJinBBangDriveService")
                 .build();
     }
 
     @Bean
-    public Sheets googleSheets() throws IOException, GeneralSecurityException {
+    public Sheets googleSheets() throws Exception {
         JsonFactory jsonFactory = GsonFactory.getDefaultInstance();
-
-        var credentials = ServiceAccountCredentials.fromStream(
-                new ClassPathResource("google.json").getInputStream() // google 서비스 인증 키 (수정필요)
-        ).createScoped(List.of(SheetsScopes.SPREADSHEETS));
+        var creds = ServiceAccountCredentials
+                .fromStream(new ClassPathResource("gcp_iam_key.json").getInputStream())
+                .createScoped(List.of(
+                        SheetsScopes.SPREADSHEETS
+                ));
 
         return new Sheets.Builder(
                 GoogleNetHttpTransport.newTrustedTransport(),
                 jsonFactory,
-                new HttpCredentialsAdapter(credentials)
+                new HttpCredentialsAdapter(creds)
         )
-                .setApplicationName("CertificateSheets")
+                .setApplicationName("JJinBBangSheetsService")
                 .build();
-    }
-
-    @Bean
-    public CertificateDriveService CertificateDriveService(Drive googleDrive) {
-        return new CertificateDriveService(googleDrive, driveFolderId);
-    }
-
-    @Bean
-    public CertificateSheetsService CertificateSheetsService(Sheets googleSheets) {
-        return new CertificateSheetsService(googleSheets, spreadsheetId);
     }
 }
