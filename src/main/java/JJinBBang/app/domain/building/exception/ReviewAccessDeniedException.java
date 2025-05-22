0001947fd2921f3f88f981e8722ac73e66ac8644
Algorithm <fighter0628@gmail.com>
2025-05-23 01:59:54 +0900

    ✨Feat[#74]: 리뷰 작성자만 삭제할 수 있는 예외 메서드 추가

--- src/main/java/JJinBBang/app/domain/building/exception/ReviewAccessDeniedException.java
+++ src/main/java/JJinBBang/app/domain/building/exception/ReviewAccessDeniedException.java
@@ -10,4 +10,8 @@ public class ReviewAccessDeniedException extends AccessDeniedGroupException {
     public static ReviewAccessDeniedException onlyAuthorCanEdit() {
         return new ReviewAccessDeniedException("작성자만 수정할 수 있습니다.");
     }
+
+    public static ReviewAccessDeniedException onlyAuthorCanDelete() {
+        return new ReviewAccessDeniedException("작성자만 삭제할 수 있습니다.");
+    }
 }
