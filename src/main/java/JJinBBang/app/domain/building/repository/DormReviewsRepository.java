fd26d2659cb52f4bcdee25cd0634a4db67573924
Seo Jiwon <wldnjstj99@gmail.com>
2025-05-23 19:41:11 +0900

    ✨ feat[#14]: 사용자 찐빵 repository 작성

--- src/main/java/JJinBBang/app/domain/building/repository/DormReviewsRepository.java
+++ src/main/java/JJinBBang/app/domain/building/repository/DormReviewsRepository.java
@@ -1,7 +1,13 @@
 package JJinBBang.app.domain.building.repository;
 
 import JJinBBang.app.domain.building.entity.DormReviews;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DormReviewsRepository extends CrudRepository<DormReviews,Long> {
+public interface DormReviewsRepository extends JpaRepository<DormReviews, Long> {
+
+    @EntityGraph(attributePaths = {"building", "building.campus"})
+    Page<DormReviews> findByUser_UserId(Long userId, Pageable pageable);
 }
