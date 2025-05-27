b7deeaa68bf593e7079d685deeda66e19e2b7d66
Algorithm <fighter0628@gmail.com>
2025-05-27 16:12:52 +0900

    ♻️ Refactor[#62]: PageRequest.getSort 를 SortType.toSort 로 대체하고 SortType enum 에 정렬 속성·방향 추가

--- src/main/java/JJinBBang/app/domain/common/enums/SortType.java
+++ src/main/java/JJinBBang/app/domain/common/enums/SortType.java
@@ -1,8 +1,22 @@
 package JJinBBang.app.domain.common.enums;
 
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Sort;
+
+@AllArgsConstructor
 public enum SortType {
-	LATEST,	// 최신순
-	LIKES,	// 좋아요순
-	STARS,	// 별점순
-	RCMND  // 추천순
+
+	// enum 상수에 [속성명, 방향]을 정의
+	LATEST("createdAt", Sort.Direction.DESC),
+	LIKES ("likesCount", Sort.Direction.DESC),
+	STARS ("rating",    Sort.Direction.DESC),
+	// 추천순는 임시로 likesCount를 기준으로 정렬
+	RCMND ("likesCount", Sort.Direction.DESC);
+
+	private final String property;
+	private final Sort.Direction direction;
+
+	public Sort toSort() {
+		return Sort.by(direction, property);
+	}
 }
