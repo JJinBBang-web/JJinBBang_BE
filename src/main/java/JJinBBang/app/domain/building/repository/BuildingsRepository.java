8e3c81907b5c54b8c91f09c2b25ade07fa444cb3
Algorithm <fighter0628@gmail.com>
2025-05-27 16:10:43 +0900

    ♻️ Refactor[#62]: BuildingsRepository에 BuildingRepositoryCustom 인터페이스 상속 추가 및 MapServiceImpl에서 BuildingsRepository로 교체

--- src/main/java/JJinBBang/app/domain/building/repository/BuildingsRepository.java
+++ src/main/java/JJinBBang/app/domain/building/repository/BuildingsRepository.java
@@ -2,10 +2,12 @@ package JJinBBang.app.domain.building.repository;
 
 import JJinBBang.app.domain.building.entity.Buildings;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-public interface BuildingsRepository extends JpaRepository<Buildings, Long> {
+@Repository
+public interface BuildingsRepository extends JpaRepository<Buildings, Long>, BuildingRepositoryCustom {
     Optional<Buildings> findByBuildingCode(String buildingCode);
 
 }
