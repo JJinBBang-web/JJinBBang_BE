474648f4650d807b6eb993f07f7cddaae4d0d4a7
seoiiwon <wldnjstj99@gmail.com>
2025-07-27 02:13:26 +0900

    🔨 refactor[#117]: controller 수정

--- src/main/java/JJinBBang/app/domain/user/controller/CertificateController.java
+++ src/main/java/JJinBBang/app/domain/user/controller/CertificateController.java
@@ -4,6 +4,7 @@ import JJinBBang.app.domain.user.dto.request.UpdateVerificationStatusDto;
 import JJinBBang.app.domain.user.entity.Users;
 import JJinBBang.app.domain.user.service.CertificateService;
 import JJinBBang.app.domain.user.service.UsersService;
+import JJinBBang.app.global.config.GoogleProperties;
 import JJinBBang.app.global.template.ResTemplate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -19,6 +20,7 @@ public class CertificateController {
 
     private final CertificateService certificateService;
     private final UsersService usersService;
+    private final GoogleProperties googleProps;
 
     // 재학증명서 업로드
     @PostMapping("/enrollment/verify")
@@ -26,10 +28,13 @@ public class CertificateController {
             @AuthenticationPrincipal Users principal,
             @RequestPart("file") MultipartFile file
     ) {
+        if (principal == null || principal.getProviderId() == null) {
+            throw new RuntimeException("사용자 인증 정보가 유효하지 않습니다.");
+        }
         Users user = usersService.findWithUniversity(principal.getProviderId());
 
-        // 대학교 이름을 기준으로 폴더 지정
-        String folderName = user.getUniversity().getUniversityName();
+        // 폴더 이름으로 타겟 폴더 지정
+        String folderName = googleProps.getDrive().getFolders().get("enrollment-integration");
 
         // 구글 드라이브에 업로드 및 링크 반환
         String fileLink = certificateService.uploadEnrollmentFileToDrive(file, folderName);
@@ -53,10 +58,14 @@ public class CertificateController {
             @AuthenticationPrincipal Users principal,
             @RequestPart("file") MultipartFile file
     ) {
+        if (principal == null || principal.getProviderId() == null) {
+            throw new RuntimeException("사용자 인증 정보가 유효하지 않습니다.");
+        }
+
         Users user = usersService.findWithUniversity(principal.getProviderId());
 
         // 구글 드라이브에 업로드 및 링크 반환
-        String folderName = user.getUniversity().getUniversityName();
+        String folderName = googleProps.getDrive().getFolders().get("admission-integration");
 
         // 구글 드라이브에 업로드 및 링크 반환
         String fileLink = certificateService.uploadAdmissionFileToDrive(file, folderName);
