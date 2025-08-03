80917f6301478e75cd6f5d983cff45b56cebd052
Junho Yu <ryuwon@gnu.ac.kr>
2025-08-03 13:23:14 +0900

    ✨ Feat[#119]: 공인중개사 조회 쿼리 추가

--- src/main/java/JJinBBang/app/domain/building/repository/custom/AgenciesRepositoryCustom.java
+++ src/main/java/JJinBBang/app/domain/building/repository/custom/AgenciesRepositoryCustom.java
@@ -5,4 +5,9 @@ import JJinBBang.app.domain.building.entity.Agencies;
 
 public interface AgenciesRepositoryCustom {
 	List<Agencies> searchAgencies(String keyword);
+
+	List<Agencies> findMarkersWithinBounds(
+		Double neLat, Double neLng,
+		Double swLat, Double swLng
+	);
 }
