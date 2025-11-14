76766d79163ec462f02b85138f07c7973d879edc
KJH <fighter0628@gmail.com>
2025-11-15 00:28:52 +0900

    ✨ Feat[#265]: AgencyLikesRepository, DormitoryFacilitiesRepository, ReviewLikesRepository 리포지토리에서 복합키 사용하도록 수정

--- src/main/java/JJinBBang/app/domain/building/repository/DormitoryFacilitiesRepository.java
+++ src/main/java/JJinBBang/app/domain/building/repository/DormitoryFacilitiesRepository.java
@@ -1,11 +1,12 @@
 package JJinBBang.app.domain.building.repository;
 
+import JJinBBang.app.domain.building.entity.DormFacilityId;
 import JJinBBang.app.domain.building.entity.DormReviews;
 import JJinBBang.app.domain.building.entity.DormitoryFacilities;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DormitoryFacilitiesRepository extends JpaRepository<DormitoryFacilities, Long> {
+public interface DormitoryFacilitiesRepository extends JpaRepository<DormitoryFacilities, DormFacilityId> {
     void deleteAllByDormitoryReview(DormReviews oldReview);
 }
--- src/main/java/JJinBBang/app/domain/building/repository/ReviewLikesRepository.java
+++ src/main/java/JJinBBang/app/domain/building/repository/ReviewLikesRepository.java
@@ -1,16 +1,17 @@
 package JJinBBang.app.domain.building.repository;
 
+import JJinBBang.app.domain.building.entity.ReviewLikeId;
 import JJinBBang.app.domain.building.entity.ReviewLikes;
 import JJinBBang.app.domain.building.entity.Reviews;
 import JJinBBang.app.domain.user.entity.Users;
 
 import org.springframework.data.jpa.repository.EntityGraph;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface ReviewLikesRepository extends CrudRepository<ReviewLikes, Long> {
+public interface ReviewLikesRepository extends JpaRepository<ReviewLikes, ReviewLikeId> {
     Optional<ReviewLikes> findByReviewAndUser(Reviews reviews, Users user);
     Optional<ReviewLikes> findByReviewIdAndUserUserId(Long reviewId, Long userId);
     Boolean existsByReviewAndUser(Reviews reviews, Users user);
